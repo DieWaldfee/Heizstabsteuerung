3a6a462551dda0a0d7848d95f862d59678ee33f7
DieWaldfee <66571311+DieWaldfee@users.noreply.github.com>
2025-09-08 14:01:35 +0200

    ac-message after MQTT reconnect
    ac-message after MQTT reconnect to optimize startup script on iobroker

--- ESP32DevKitV4/Heizstabsteuerung.ino
+++ ESP32DevKitV4/Heizstabsteuerung.ino
@@ -1620,6 +1620,10 @@ void checkMQTTconnetion() {
       thermalLimit = 0;
     rc = xSemaphoreGive(mutexStatus);
     assert(rc == pdPASS);
+    //reconnect zurückmelden
+    mqttTopic = MQTT_SERIAL_PUBLISH_BASIS + String("ac");
+    mqttPayload = String("MQTT reconnect durchgeführt!");
+    mqttClient.publish(mqttTopic.c_str(), mqttPayload.c_str());
   }
   mqttClient.loop();
 }
